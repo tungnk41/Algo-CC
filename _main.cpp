c5c387cc9f6652e2d9b65511fd17538a360405ac
tung6.nguyen <tung6.nguyen@lge.com>
2023-06-21 14:21:02 +0700

    add file

---
+++ _main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = (a), _b_ = (b); i < _b_; i++)
+#define ROF(i, a, b) for (int i = (a), _b_ = (b); i > _b_; i--)
+#define IT(i, v) for (auto i = (v).begin(); i != (v).end(); ++i)
+#define ALL(v) (v).begin(), (v).end()
+#define MS(v) memset(v, 0, sizeof(v))
+
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int, int> PII;
+
+template <typename T> vector<T> &operator+=(vector<T> &v, T x)
+{
+    v.push_back(x);
+    return v;
+}
+
+int main() {
+
+   return 0;
+}
